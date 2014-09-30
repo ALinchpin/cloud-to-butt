9ef855a71ebd99867a5a3f71677a157a1c7ec4bd
ALinchpin <jklemsz@gmail.com>
2014-09-30 11:43:26 -0500

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,15 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bdreg\b/g, "greg");
+	v = v.replace(/\bdreg\b/g, "Greg");
+	v = v.replace(/\bDREG\b/g, "GREG");
+	v = v.replace(/\bvandal\b/g, "randal");
+	v = v.replace(/\bVandal\b/g, "Randal");
+	v = v.replace(/\bVANDAL\b/g, "RANDAL");
+	v = v.replace(/\bshank\b/g, "skank");
+	v = v.replace(/\bShank\b/g, "Skank");
+	v = v.replace(/\bSHANK\b/g, "SKANK");
 	
 	textNode.nodeValue = v;
 }
